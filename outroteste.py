ed4f534a16cc38b65f3158b66fc3a333edd14149
Roberto Junqueira <roberto.junqueira@me.com>
2025-09-25 10:39:15 -0300

    evoluindo no gihub....

--- outroteste.py
+++ outroteste.py
@@ -2,3 +2,11 @@
 
 print("consegui linkar o github com o vscode")
 
+Print("agora só falta aprender a usar o github direito. Ainda sou novato no assunto")
+
+print("mas já é um começo")
+
+print("vamos ver se consigo fazer o commit e o push")
+
+
+
